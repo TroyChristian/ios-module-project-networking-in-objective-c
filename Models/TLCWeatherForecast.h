d6b0c403769e3cdf8f34fe25845f2c9ef8d7fd63
Troy Christian <apostasy8@gmail.com>
2020-02-19 23:48:21 -0500

    Add TACWeatherForecast

---
+++ Models/TLCWeatherForecast.h
@@ -0,0 +1,17 @@
+//
+//  TLCWeatherForecast.h
+//  DailyWeather
+//
+//  Created by Lambda_School_Loaner_219 on 2/19/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TLCWeatherForecast : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Models/TLCWeatherForecast.m
@@ -0,0 +1,13 @@
+//
+//  TLCWeatherForecast.m
+//  DailyWeather
+//
+//  Created by Lambda_School_Loaner_219 on 2/19/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "TLCWeatherForecast.h"
+
+@implementation TLCWeatherForecast
+
+@end
